2240ea6b96a87962f2768b6afc167e6320dacc23
Dries Harnie <dries.harnie@vub.ac.be>
2012-03-02 14:44:57 +0100

    Start ball at a nice angle

--- src/edu/vub/portalpong/objects/Ball.java
+++ src/edu/vub/portalpong/objects/Ball.java
@@ -17,13 +17,21 @@ public class Ball extends GameObject{
 	public Ball(int x, int y) {
 		super(x,y);
 		
-		double angle = 2 * Math.PI * new Random().nextDouble();
-		this.dx = Math.cos(angle);
-		this.dy = Math.sin(angle);
+		double angle = 0;
+		do {
+			angle = 2 * Math.PI * new Random().nextDouble();
+			this.dx = Math.cos(angle);
+			this.dy = Math.sin(angle);
+		} while (! goodAngle(angle));
 		
 		this.paint = new Paint();
 		this.paint.setColor(Color.WHITE);
 		this.size = 40 / 2;
+	}
+
+	private boolean goodAngle(double angle) {
+		return Math.abs(dx) < 0.90 && Math.abs(dx) > 0.10
+		    && Math.abs(dy) < 0.90 && Math.abs(dy) > 0.10;
 	}
 
 	public void draw(Canvas c) {
